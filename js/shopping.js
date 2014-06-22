4b8c99929d3fbd99db07faa21b0759a20019d2a8
wgao4000 <wgao4000@gmail.com>
2014-06-22 18:43:15 -0400

    newest update

--- js/shopping.js
+++ js/shopping.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	var allitems=[];
 	var allprices=[];
 	var allquantities=[];
-	$('.enteritem').keydown(function(event){
-	    var flag1,flag2=true;
-      if(event.which==13){
+    var flag=true;
+  $('.enteritem').keydown(function(event){
+	    if(event.which==13){
 			    event.preventDefault(); 
 			    new_item=$('.enteritem').val();
           allitems.push("."+new_item+"B");
@@ -13,27 +13,29 @@ $(document).ready(function(){
           allquantities.push("#"+new_item+"Q");
           $('#shopping_list').append("<div class='rows"+new_item+"'><span class='newitems'><input type='checkbox' name='shoplist',id='check"+new_item+"' checked value="+new_item+">"+new_item.substring(0,1).toUpperCase()+new_item.substring(1)+"</span><label for='"+new_item+"P'>Price $</label> <input type='text' id='"+new_item+"P'> <label for='"+new_item+"Q'>Quantity</label> <input type='text' id='"+new_item+"Q'><button class='"+new_item+"B'>permanently remove</button></div><br>");
        }
+    
        $("button").click(function(){
           var buttonvalue=String($(this).attr("class"));
-
+          if(flag){
           $.each(allitems,function(item,value){
-          
-        	  if(String(value)==("."+buttonvalue)){
+             if(String(value)==("."+buttonvalue)){
+              alert("#"+String(value).substring(1,String(value).length-1)+"P");
+              alert("tgtg1"+$("#"+String(value).substring(1,String(value).length-1)+"P").val());
               var selectprice=$("#"+String(value).substring(1,String(value).length-1)+"P").val();
               var selectquantity=$("#"+String(value).substring(1,String(value).length-1)+"Q").val();
+              alert("tgtg1"+(String(value).substring(1,String(value).length-1)+"Q"));
+              alert("tgtg"+$("#"+String(value).substring(1,String(value).length-1)+"Q").val());
               total=total-parseFloat(selectprice)*parseFloat(selectquantity);
               $(".rows"+String(value).substring(1,String(value).length-1)).remove();
               $(".sales").replaceWith("<div class='sales'><br>Subtotal: $"+total.toFixed(2)+"<br>Tax: [MA @ 6.25%]: $"+(total*0.0625).toFixed(2)+"<br>Purchase Price: $"+(total*1.0625).toFixed(2)+"</div>");
-              flag1=false;
-              flag2=false;
-              return false;
-            }
-              flag1=false;
-              return flag1; 
-          });
-            // return flag2;
-        });
-        $.each(allprices,function(item,value){ 
+              flag=false;
+              }
+             return flag;
+           });
+         }
+       });
+       flag=true;
+       $.each(allprices,function(item,value){ 
           $(String(value)).keydown(function(event){
 	           if(event.which==13){
 			         event.preventDefault(); 
@@ -47,12 +49,12 @@ $(document).ready(function(){
 	           if(event.which==13){
 			         event.preventDefault(); 
 			         quantity_per=$(String(value)).val();
-               
+               alert(quantity_per);
            // alert($("#checkthis").is(':checked'));
            //  if($("#checkthis").is(':checked')){
           	   total=total+parseFloat(price_per)*parseFloat(quantity_per);
-          	   
-               $(".sales").replaceWith("<div class='sales'><br>Subtotal: $"+total.toFixed(2)+"<br>Tax: [MA @ 6.25%]: $"+(total*0.0625).toFixed(2)+"<br>Purchase Price: $"+(total*1.0625).toFixed(2)+"</div>");
+          	   alert(total);
+          	   $(".sales").replaceWith("<div class='sales'><br>Subtotal: $"+total.toFixed(2)+"<br>Tax: [MA @ 6.25%]: $"+(total*0.0625).toFixed(2)+"<br>Purchase Price: $"+(total*1.0625).toFixed(2)+"</div>");
              }
          // $(".sales").replaceWith("<div class='sales'><br>Subtotal: "+total.toFixed(2)+"<br>Tax: [MA @ 6.25%]: "+(total*0.0625).toFixed(2)+"<br>Purchase Price: "+(total*1.0625).toFixed(2)+"</div>");
 	       });
